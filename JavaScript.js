d7d80f7bac07ba94bc92e8b23228b678eeed0c59
Nadia Batchelor <nadabdesigns@gmail.com>
2025-03-24 14:29:56 -0400

    Home page updates

---
+++ JavaScript.js
@@ -0,0 +1,23 @@
+// tracking a div on top of the mouse
+// event liser is appoy to the whole document
+// and in that case if the event lisener is ture run the following
+// this is where e is defined and the prameter that the data is getting sent to
+document.addEventListener('mousemove', (e) => {
+	
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+	console.log(mouseX)
+	console.log(mouseY)
+
+})
+
+// add a trail
+
+// varbile that keeps track of the the trail
+
+// add an array of divs
+
+
+// reference the DOM
+
+
