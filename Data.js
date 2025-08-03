5e946f782dcaf3acb01b926cdfcc44200c5848cd
DonGantt <ig246@yahoo.com>
2025-08-03 02:14:59 -0400

    Data.js: Code linting and formatting fixes; index.html: changes the id names and classes around to fit semantically; script.js: comments out broken variable calls to properly call the functions and wraps it in a dom content loaded wrapper to add section functionality; style.css: adds a new class

--- Data.js
+++ Data.js
@@ -1,24 +1,22 @@
-
-console.log('hello')
-let channelSlug = 'project-4-birds' // The “slug” is just the end of the URL
-
+console.log("hello");
+let channelSlug = "project-4-birds"; // The “slug” is just the end of the URL
 
 // Then our big function for specific-block-type rendering:
 let renderBlock = (block) => {
-	// To start, a shared `ul` where we’ll insert all our blocks
-	let channelBlocks = document.querySelector('#channel-blocks')
-	let highlightClass = 'highlight' // “Strings” (like a class name) are wrapped in quotes.
-	let textBlock = document.querySelector('#bigSun') // Any selector.
-	let switchButton = document.querySelector('#bigSun') // change this if you want somthing elese to trigger it 
+  // To start, a shared `ul` where we’ll insert all our blocks
+  let channelBlocks = document.querySelector("#channel-blocks");
+  let highlightClass = "highlight"; // “Strings” (like a class name) are wrapped in quotes.
+  let textBlock = document.querySelector("#bigSun"); // Any selector.
+  let switchButton = document.querySelector("#bigSun"); // change this if you want somthing elese to trigger it
 
-	switchButton.onclick = () => { // “Listen” for clicks.
-		textBlock.classList.toggle(highlightClass) // Toggle the class!
-	}
+  switchButton.onclick = () => {
+    // “Listen” for clicks.
+    textBlock.classList.toggle(highlightClass); // Toggle the class!
+  };
 
-	// Links!
-	if (block.class == 'Link') {
-		let linkItem =
-			`
+  // Links!
+  if (block.class == "Link") {
+    let linkItem = `
 			<li>
 				<p><em>Link</em></p>
 				<picture>
@@ -30,34 +28,28 @@ let renderBlock = (block) => {
 				${block.description_html}
 				<p><a href="${block.source.url}">See the original ↗</a></p>
 			</li>
-			`
-		channelBlocks.insertAdjacentHTML('beforeend', linkItem)
-	}
+			`;
+    channelBlocks.insertAdjacentHTML("beforeend", linkItem);
+  }
 
+  // Want to click on item and then have it appear in the div when it's an image
+  // set the class div for the images
+  // assign div with a class link
+  // create div logic
+  // Set up our variables.
 
+  // example from class
 
-	// Want to click on item and then have it appear in the div when it's an image
-	// set the class div for the images 
-	// assign div with a class link 
-	// create div logic
-	// Set up our variables.
+  //  let imagelisten
+  //  if imageName is clicked then display the same name of that imgage in the div
 
-	// example from class
+  // first let see if we can get image inside the div
 
+  // Images Test!
+  else if (block.class == "Image") {
+    // let can be changed when ever i want and const will be for forever
 
-	//  let imagelisten 
-	//  if imageName is clicked then display the same name of that imgage in the div
-
-	// first let see if we can get image inside the div
-	
-	// Images Test!
-	else if (block.class == 'Image') {
-		// let can be changed when ever i want and const will be for forever
-
-
-		let imageItem =
-
-			`
+    let imageItem = `
 			<div id="#all-blocks">
 			${block.image.large.url}
 			${block.title}
@@ -68,32 +60,26 @@ let renderBlock = (block) => {
 
 	
 			
-			`
-		// …up to you!
-	}
+			`;
+    // …up to you!
+  }
 
+  // Images!
+  else if (block.class == "Image") {
+    // let can be changed when ever i want and const will be for forever
+    console.log(block);
 
+    let imageItem = `<p class="itemName">${block.title}`;
+    // …up to you!
+  }
 
-	// Images!
+  // step one make the titles of the blocks appear in the text
 
-	else if (block.class == 'Image') {
-		// let can be changed when ever i want and const will be for forever
-		console.log(block)
-		
-
-		let imageItem =`<p class="itemName">${block.title}`
-		// …up to you!
-	}
-
-
-
-	// step one make the titles of the blocks appear in the text
-
-	// Text!
-	// make styling for text 
-	// make a div
-	else if (block.class == 'Text') {
-		`
+  // Text!
+  // make styling for text
+  // make a div
+  else if (block.class == "Text") {
+    `
 			<div class= textBlock>
 				<p><em>Link</em></p>
 				<picture>
@@ -106,108 +92,106 @@ let renderBlock = (block) => {
 				<p><a href="${block.source.url}">See the original ↗</a></p>
 			</div>
 			
-			`
+			`;
 
-		// …up to you!
-	}
+    // …up to you!
+  }
 
+  // Uploaded (not linked) media…
+  else if (block.class == "Attachment") {
+    let attachment = block.attachment.content_type; // Save us some repetition
 
-
-	// Uploaded (not linked) media…
-	else if (block.class == 'Attachment') {
-		let attachment = block.attachment.content_type // Save us some repetition
-
-		// Uploaded videos!
-		if (attachment.includes('video')) {
-			// …still up to you, but we’ll give you the `video` element:
-			let videoItem =
-				`
+    // Uploaded videos!
+    if (attachment.includes("video")) {
+      // …still up to you, but we’ll give you the `video` element:
+      let videoItem = `
 				<li>
 					<p><em>Video</em></p>
 					<video controls src="${block.attachment.url}"></video>
 				</li>
-				`
-			channelBlocks.insertAdjacentHTML('beforeend', videoItem)
-			// More on video, like the `autoplay` attribute:
-			// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/video
-		}
+				`;
+      channelBlocks.insertAdjacentHTML("beforeend", videoItem);
+      // More on video, like the `autoplay` attribute:
+      // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/video
+    }
 
-		// Uploaded PDFs!
-		else if (attachment.includes('pdf')) {
-			// …up to you!
-		}
+    // Uploaded PDFs!
+    else if (attachment.includes("pdf")) {
+      // …up to you!
+    }
 
-		// Uploaded audio!
-		else if (attachment.includes('audio')) {
-			// …still up to you, but here’s an `audio` element:
-			let audioItem =
-				`
+    // Uploaded audio!
+    else if (attachment.includes("audio")) {
+      // …still up to you, but here’s an `audio` element:
+      let audioItem = `
 				<li>
 					<p><em>Audio</em></p>
 					<audio controls src="${block.attachment.url}"></video>
 				</li>
-				`
-			channelBlocks.insertAdjacentHTML('beforeend', audioItem)
-			// More on audio: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/audio
-		}
-	}
+				`;
+      channelBlocks.insertAdjacentHTML("beforeend", audioItem);
+      // More on audio: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/audio
+    }
+  }
 
-	// Linked media…
-	else if (block.class == 'Media') {
-		let embed = block.embed.type
+  // Linked media…
+  else if (block.class == "Media") {
+    let embed = block.embed.type;
 
-		// Linked video!
-		if (embed.includes('video')) {
-			// …still up to you, but here’s an example `iframe` element:
-			let linkedVideoItem =
-				`
+    // Linked video!
+    if (embed.includes("video")) {
+      // …still up to you, but here’s an example `iframe` element:
+      let linkedVideoItem = `
 				<li>
 					<p><em>Linked Video</em></p>
 					${block.embed.html}
 				</li>
-				`
-			channelBlocks.insertAdjacentHTML('beforeend', linkedVideoItem)
-			// More on iframe: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/iframe
-		}
+				`;
+      channelBlocks.insertAdjacentHTML("beforeend", linkedVideoItem);
+      // More on iframe: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/iframe
+    }
 
-		// Linked audio!
-		else if (embed.includes('rich')) {
-			// …up to you!
-		}
-	}
-}
-
-
+    // Linked audio!
+    else if (embed.includes("rich")) {
+      // …up to you!
+    }
+  }
+};
 
 // It‘s always good to credit your work:
-let renderUser = (user, container) => { // You can have multiple arguments for a function!
-	let userAddress =
-		`
+let renderUser = (user, container) => {
+  // You can have multiple arguments for a function!
+  let userAddress = `
 		<address>
 			<img src="${user.avatar_image.display}">
 			<h3>${user.first_name}</h3>
 			<p><a href="https://are.na/${user.slug}">Are.na profile ↗</a></p>
 		</address>
-		`
-	container.insertAdjacentHTML('beforeend', userAddress)
-}
+		`;
+  container.insertAdjacentHTML("beforeend", userAddress);
+};
 
 // Now that we have said what we can do, go get the data:
-fetch(`https://api.are.na/v2/channels/${channelSlug}?per=100`, { cache: 'no-store' })
-	.then((response) => response.json()) // Return it as JSON data
-	.then((data) => { // Do stuff with the data
-		console.log(data) // Always good to check your response!
+fetch(`https://api.are.na/v2/channels/${channelSlug}?per=100`, {
+  cache: "no-store",
+})
+  .then((response) => response.json()) // Return it as JSON data
+  .then((data) => {
+    // Do stuff with the data
+    console.log(data); // Always good to check your response!
 
-		placeChannelInfo(data) // Pass the data to the first function
+    placeChannelInfo(data); // Pass the data to the first function
 
-		// Loop through the `contents` array (list), backwards. Are.na returns them in reverse!
-		data.contents.reverse().forEach((block) => {
-			// console.log(block) // The data for a single block
-			renderBlock(block) // Pass the single block data to the render function
-		})
-		// lets you stor things in the name 
-		// Also display the owner and collaborators:
-		let channelUsers = document.querySelector('#channel-users') // Show them together
-		data.collaborators.forEach((collaborator) => renderUser(collaborator, channelUsers))
-		renderUser(data.user, channelUsers)
-	})
+    // Loop through the `contents` array (list), backwards. Are.na returns them in reverse!
+    data.contents.reverse().forEach((block) => {
+      // console.log(block) // The data for a single block
+      renderBlock(block); // Pass the single block data to the render function
+    });
+    // lets you stor things in the name
+    // Also display the owner and collaborators:
+    let channelUsers = document.querySelector("#channel-users"); // Show them together
+    data.collaborators.forEach((collaborator) =>
+      renderUser(collaborator, channelUsers)
+    );
+    renderUser(data.user, channelUsers);
+  });
--- script.js
+++ script.js
@@ -1,81 +1,107 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // mobile here
+  // Set up our variables.
+  let classChange = "highlight";
+  // let dropDown = document.querySelector("section");
+  let switchButton = document.querySelector("#example");
 
+  // change.classList.add('class-name','one','two')
+  // change.classList.remove('rest','one')
+  // change.classList.toggle('rest')
 
+  // Dots scroll here
+  // console.log("Divs found:", sections.length);
 
+  // Select all divs you want to observe (better to use a class)
+  // const sections = document.querySelectorAll(".scroll-section");
+  // console.log("Divs found:", sections.length);
 
-// mobile here
-// Set up our variables.
-let classChange = 'highlight';
-let dropDown = document.querySelector('section')
-let switchButton = document.querySelector('#example')
+  // const addActiveClass = (entries) => {
+  //   entries.forEach((entry) => {
+  //     if (entry.isIntersecting) {
+  //       console.log("Visible div:", entry.target);
+  //       entry.target.classList.add("active"); // Example: add a class
+  //     } else {
+  //       entry.target.classList.remove("active");
+  //     }
+  //   });
+  // };
 
+  // const options = {
+  //   threshold: 0.5,
+  // };
 
-// change.classList.add('class-name','one','two')
-// change.classList.remove('rest','one')
-// change.classList.toggle('rest')
+  // const observer = new IntersectionObserver(addActiveClass, options);
 
+  // Attach observer to each div
+  // sections.forEach((div) => observer.observe(div));
 
+  // figure out how to click one
+  // change it to white
+  // scroll dow
+  // get rid of the other
+  // the fist thing is if and the second thing is elese if
 
+  const navButtons = document.querySelectorAll(".dot");
 
-// Dots scroll here
-console.log("Divs found:", sections.length);
+  function highlightButton(targetId) {
+    navButtons.forEach((button) => {
+      if (button.dataset.target === targetId) {
+        button.classList.add("active");
+      } else {
+        button.classList.remove("active");
+      }
+    });
+  }
 
-
-// Select all divs you want to observe (better to use a class)
-const sections = document.querySelectorAll(".scroll-section");
-console.log("Divs found:", sections.length);
-
-const addActiveClass = (entries) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      console.log("Visible div:", entry.target);
-      entry.target.classList.add("active"); // Example: add a class
-    } else {
-      entry.target.classList.remove("active");
+  const buttonObserver = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const elementId = entry.target.id;
+          highlightButton(elementId);
+        }
+      });
+    },
+    {
+      threshold: 0.5,
+      rootMargin: "-100px 0px",
     }
+  );
+
+  const buttonSections = document.querySelectorAll(".section");
+
+  buttonSections.forEach((section) => {
+    buttonObserver.observe(section);
   });
-};
 
-const options = {
-  threshold: 0.5
-};
+  navButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const targetId = button.dataset.target;
+      const targetElement = document.getElementById(targetId);
+      if (targetElement) {
+        targetElement.scrollIntoView({
+          behavior: "smooth",
+          block: "center",
+        });
+      }
+    });
+  });
 
-const observer = new IntersectionObserver(addActiveClass, options);
-
-// Attach observer to each div
-sections.forEach(div => observer.observe(div));
-
-
-
-// figure out how to click one 
-// change it to white 
-// scroll dow
-// get rid of the other
-// the fist thing is if and the second thing is elese if
-
-
-
-
-
-// tracking a div on top of the mouse
-// event liser is appoy to the whole document
-// and in that case if the event lisener is ture run the following
-// this is where e is defined and the prameter that the data is getting sent to
-document.addEventListener('mousemove', (e) => {
-	
+  // tracking a div on top of the mouse
+  // event liser is appoy to the whole document
+  // and in that case if the event lisener is ture run the following
+  // this is where e is defined and the prameter that the data is getting sent to
+  document.addEventListener("mousemove", (e) => {
     const mouseX = e.clientX;
     const mouseY = e.clientY;
-	console.log(mouseX)
-	console.log(mouseY)
+  });
 
-})
+  // add a trail
 
-// add a trail
+  // varbile that keeps track of the the trail
 
-// varbile that keeps track of the the trail
+  // add an array of divs
 
-// add an array of divs
-
-
-// reference the DOM
-
-
+  // reference the DOM
+});
